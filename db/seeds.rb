2db765ec64ba81ad543a0cfc2951dc0e5da53070
PEDRO <pedro.araya.labra@gmail.com>
2024-11-24 20:32:15 -0300

    add faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+ClimbingPlace.destroy_all
+
+10.times do
+  latitude = 32.0 + rand * (38.0 - 32.0) # Random number between 37.0 and 38.0
+  longitude = -90.0 + rand * (90.0 - 80.0) # Random number between -122.0 and -121.0
+
+  ClimbingPlace.new(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraphs(number: 10).join(''),
+    latitude: latitude,
+    longitude: longitude  
+  ).save(validate: false)
+end

2ff0fe1d844ab6fe459797b859787daa3fe10418
PEDRO <pedro.araya.labra@gmail.com>
2024-11-25 17:16:09 -0300

    upgrade google map window content

--- app/helpers/climbing_places_helper.rb
+++ app/helpers/climbing_places_helper.rb
@@ -1,2 +1,5 @@
 module ClimbingPlacesHelper
+  def google_maps_url(latitude, longitude)
+    "https://www.google.com/maps?q=#{latitude},#{longitude}"
+  end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,6 +5,10 @@ export default class extends Controller {
   static values = {
     latitude: Number,
     longitude: Number,
+    thumbnail: String, 
+    name: String,
+    address: String,
+    link: String
   }
 
   connect() {
@@ -22,16 +26,15 @@ export default class extends Controller {
       map: map,
       title: "Location Name",
     });
-  
 
     // Define the content for the custom card
     const infoWindowContent = `
       <div style="font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5; max-width: 200px;">
-        <img src="https://www.revoltclimbing.com/wp-content/uploads/2020/01/01a_IMG_7677_1_slrep_dcl_lev_v_16x9_l_lev-1-2048x1152.jpg" alt="HTML5 Doctor Logo" width="95%"/>
+        <img src="${this.thumbnailValue}" alt="HTML5 Doctor Logo" width="95%"/>
         <br />
-        <strong>Location Name</strong><br>
-        123 Main Street, City, Country<br>
-        <a href="https://example.com" target="_blank">More Info</a>
+        <strong>${this.nameValue}</strong><br>
+        ${this.addressValue}
+        <a href="${this.linkValue}" target="_blank">More Info</a>
       </div>
     `;
 

a17b3a2d1d0dd5072a70009a2f4952702bdb5fcb
PEDRO <pedro.araya.labra@gmail.com>
2024-11-29 00:10:04 -0300

    add viewer.js to zoom images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,15 @@ import * as bootstrap from "bootstrap"
 
 import "trix"
 import "@rails/actiontext"
+
+import "viewerjs";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(Viewer)
+  const gallery = document.getElementById('gallery'); // Replace 'gallery' with your element ID
+  const viewer = new Viewer(gallery, {
+    navbar: false,     // Hide the navbar
+    toolbar: false,    // Hide the toolbar
+    movable: false,
+  });
+});

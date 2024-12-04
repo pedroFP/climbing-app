c2acacd74638e15a895e14c2dd46f459732eb3ee
PEDRO <pedro.araya.labra@gmail.com>
2024-12-03 23:21:14 -0300

    feat: :lipstick: add country flags icons on the climbing_places

--- app/helpers/climbing_places_helper.rb
+++ app/helpers/climbing_places_helper.rb
@@ -8,4 +8,8 @@ module ClimbingPlacesHelper
   def countries_names_for_select
     ISO3166::Country.all.map { |c| [ c.iso_short_name, c.iso_short_name ] }
   end
+
+  def country_flag(country_code)
+    country_code.upcase.chars.map { |char| (char.ord + 127397).chr(Encoding::UTF_8) }.join
+  end
 end

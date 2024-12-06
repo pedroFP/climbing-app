781bb37470dd603263ccf67d9a1dabc60d5f777a
PEDRO <pedro.araya.labra@gmail.com>
2024-12-05 21:33:45 -0300

    feat: :wrench: setup sender email for development environmet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,17 +72,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: "<example@mail.com>" }
+  config.action_mailer.default_options = { from: "<#{ Rails.application.credentials.smtp.default_sender }>" }
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "yourdomain.com",
-    user_name: Rails.application.credentials.smtp.user_name,
-    password: Rails.application.credentials.smtp.password,
-    authentication: "plain",
+    address:              "smtp-relay.brevo.com",
+    port:                 587,
+    domain:               "yourdomain.com", # Replace with your domain
+    user_name:            Rails.application.credentials.smtp.user_name,
+    password:             Rails.application.credentials.smtp.password,
+    authentication:       :login,
     enable_starttls_auto: true
   }
 end

4c2026801c993440a3c6534092db3e40e0e1a67a
PEDRO <pedro.araya.labra@gmail.com>
2024-12-11 23:18:59 -0300

    fix: :wrench: setup devise sender and default action mailer sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_options = { from: "<#{ Rails.application.credentials.smtp.no_reply_sender }>" }
 
   config.action_mailer.smtp_settings = {
     address:              "smtp-relay.brevo.com",

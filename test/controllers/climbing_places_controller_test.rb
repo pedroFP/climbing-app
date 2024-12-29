bdcd98a169112c7900df919c744dd6926247c522
PEDRO <pedro.araya.labra@gmail.com>
2024-12-29 18:45:25 -0300

    test: :white_check_mark: update tests

--- test/controllers/climbing_places_controller_test.rb
+++ test/controllers/climbing_places_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class ClimbingPlacesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @climbing_place = climbing_places(:one)
+    @user = @climbing_place.user
   end
 
   test "should get index" do
@@ -11,11 +12,15 @@ class ClimbingPlacesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in @user
+
     get new_climbing_place_url
     assert_response :success
   end
 
   test "should create climbing_place" do
+    sign_in @user
+
     assert_difference("ClimbingPlace.count") do
       post climbing_places_url, params: { climbing_place: { address: @climbing_place.address, latitude: @climbing_place.latitude, longitude: @climbing_place.longitude, name: @climbing_place.name } }
     end
@@ -29,16 +34,22 @@ class ClimbingPlacesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    sign_in @user
+
     get edit_climbing_place_url(@climbing_place)
     assert_response :success
   end
 
   test "should update climbing_place" do
+    sign_in @user
+
     patch climbing_place_url(@climbing_place), params: { climbing_place: { address: @climbing_place.address, latitude: @climbing_place.latitude, longitude: @climbing_place.longitude, name: @climbing_place.name } }
     assert_redirected_to climbing_place_url(@climbing_place)
   end
 
   test "should destroy climbing_place" do
+    sign_in @user
+
     assert_difference("ClimbingPlace.count", -1) do
       delete climbing_place_url(@climbing_place)
     end
